7e1f888cce74e489fd6c183fb16648f0e036675d
RICHAR-SL <richarsantivalez@gmail.com>
2026-05-23 16:34:38 -0500

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/tarea6_seminario/sw.js', { scope: '/tarea6_seminario/' })})}
