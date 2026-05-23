5070dfc3b95847e71c9480b7fcd2df8600a48e09
RICHAR-SL <richarsantivalez@gmail.com>
2026-05-23 17:50:08 -0500

    Corrigiendo rutas de iconos para el manifest

--- vite.config.js
+++ vite.config.js
@@ -13,8 +13,8 @@ export default defineConfig({
         short_name: 'IATranslate',
         theme_color: '#ffffff',
         icons: [
-          { src: '/icon-192.png', sizes: '192x192', type: 'image/png' },
-          { src: '/icon-512.png', sizes: '512x512', type: 'image/png' }
+          { src: '/tarea6_seminario/icon-192.png', sizes: '192x192', type: 'image/png' },
+          { src: '/tarea6_seminario/icon-512.png', sizes: '512x512', type: 'image/png' }
         ]
       }
     })
